22c33e5bba78b243e5586ff9a1b8f2688a4b4d05
whateveruwant <qkrdbals1630@naver.com>
2017-07-12 17:14:56 +0900

    fragment에서 activity전환

--- app/src/layout/RecommendFragment.java
+++ app/src/layout/RecommendFragment.java
@@ -8,25 +8,22 @@ import android.support.v4.view.ViewCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
+import com.papcon.papcon.LoginActivity;
 import com.papcon.papcon.MainActivity;
 import com.papcon.papcon.ManagementActivity;
 import com.papcon.papcon.R;
-import com.papcon.papcon.User;
-import com.papcon.papcon.UserListAdapter;
-
-import org.json.JSONArray;
-import org.json.JSONObject;
+import com.papcon.papcon.RegisterActivity;
 
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
 
 import butterknife.ButterKnife;
 
@@ -65,6 +62,56 @@ public class RecommendFragment extends Fragment {
                 menuItems
         );
         listView.setAdapter(listViewAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                new BackgroundTask().execute();
+            }
+        });
         return rootView;
+    }
+
+    class BackgroundTask extends AsyncTask<Void, Void, String> {
+        String target;
+
+        @Override
+        protected void onPreExecute() {
+            target = "http://dbals1630.cafe24.com/List.php";
+        }
+
+        @Override
+        protected String doInBackground(Void... params) {
+            try{
+                URL url = new URL(target);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                InputStream inputStream = httpURLConnection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                String temp;
+                StringBuilder stringBuilder = new StringBuilder();
+                while((temp = bufferedReader.readLine()) != null){
+                    stringBuilder.append(temp + "\n");
+                }
+                bufferedReader.close();
+                inputStream.close();
+                httpURLConnection.disconnect();
+                return stringBuilder.toString().trim();
+            }
+            catch(Exception e){
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        public void onProgressUpdate(Void... values) {
+            super.onProgressUpdate(values);
+        }
+
+        @Override
+        public void onPostExecute(String result) {
+            Intent intent = new Intent(getActivity(), ManagementActivity.class);
+            intent.putExtra("userList", result);
+            getActivity().startActivity(intent);
+        }
     }
 }
