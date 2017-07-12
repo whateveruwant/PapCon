72d5dbe837e12c0fa5168cb0d4fd83e0580fc330
whateveruwant <qkrdbals1630@naver.com>
2017-07-12 16:29:56 +0900

    추천보기에 리스트 추가

--- app/src/com/papcon/papcon/MainActivity.java
+++ app/src/com/papcon/papcon/MainActivity.java
@@ -1,5 +1,7 @@
 package com.papcon.papcon;
 
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
@@ -18,6 +20,17 @@ import android.widget.Toast;
 
 import com.astuetz.PagerSlidingTabStrip;
 import com.readystatesoftware.systembartint.SystemBarTintManager;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -81,16 +94,7 @@ public class MainActivity extends ActionBarActivity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
-//
-    //@Override
-    //public boolean onOptionsItemSelected(MenuItem item) {
-    //    switch (item.getItemId()) {
-    //        case R.id.action_contact:
-    //            QuickContactFragment.newInstance().show(getSupportFragmentManager(), "QuickContactFragment");
-    //            return true;
-    //    }
-    //    return super.onOptionsItemSelected(item);
-    //}
+
 
     private void changeColor(int newColor) {
         tabs.setBackgroundColor(newColor);
@@ -163,49 +167,4 @@ public class MainActivity extends ActionBarActivity {
             return null;
         }
     }
-
-    //class BackgroundTask extends AsyncTask<Void, Void, String> {
-    //    String target;
-//
-    //    @Override
-    //    protected void onPreExecute() {
-    //        target = "http://dbals1630.cafe24.com/List.php";
-    //    }
-//
-    //    @Override
-    //    protected String doInBackground(Void... params) {
-    //        try{
-    //            URL url = new URL(target);
-    //            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
-    //            InputStream inputStream = httpURLConnection.getInputStream();
-    //            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-    //            String temp;
-    //            StringBuilder stringBuilder = new StringBuilder();
-    //            while((temp = bufferedReader.readLine()) != null){
-    //                stringBuilder.append(temp + "\n");
-    //            }
-    //            bufferedReader.close();
-    //            inputStream.close();
-    //            httpURLConnection.disconnect();
-    //            return stringBuilder.toString().trim();
-    //        }
-    //        catch(Exception e){
-    //            e.printStackTrace();
-    //        }
-    //        return null;
-    //    }
-//
-    //    @Override
-    //    public void onProgressUpdate(Void... values) {
-    //        super.onProgressUpdate(values);
-    //    }
-//
-    //    @Override
-    //    public void onPostExecute(String result) {
-    //        Intent intent = new Intent(MainActivity.this, ManagementActivity.class);
-    //        intent.putExtra("userList", result);
-    //        MainActivity.this.startActivity(intent);
-    //        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
-    //    }
-    //}
 }
--- app/src/layout/RecommendFragment.java
+++ app/src/layout/RecommendFragment.java
@@ -1,26 +1,38 @@
 package layout;
 
-import android.content.Context;
-import android.net.Uri;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import com.papcon.papcon.MainActivity;
+import com.papcon.papcon.ManagementActivity;
 import com.papcon.papcon.R;
+import com.papcon.papcon.User;
+import com.papcon.papcon.UserListAdapter;
 
-import butterknife.BindView;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
 import butterknife.ButterKnife;
 
 public class RecommendFragment extends Fragment {
 
     private static final String ARG_NAME = "recommend";
-
-    @BindView(R.id.textView)
-    TextView textView;
 
     public static RecommendFragment newInstance() {
         RecommendFragment fragment = new RecommendFragment();
@@ -38,10 +50,21 @@ public class RecommendFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootview = inflater.inflate(R.layout.fragment_recommend, container, false);
-        ButterKnife.bind(this, rootview);
-        ViewCompat.setElevation(rootview, 50);
-        textView.setText("추천목록");
-        return rootview;
+        View rootView = inflater.inflate(R.layout.fragment_recommend, container, false);
+        ButterKnife.bind(this, rootView);
+        ViewCompat.setElevation(rootView, 50);
+
+        String[] menuItems = {"안녕", "하이", "오하이오", "할라",
+                "안녕", "하이", "오하이오", "할라",
+                "안녕", "하이", "오하이오", "할라",
+                "안녕", "하이", "오하이오", "할라"};
+        ListView listView = (ListView) rootView.findViewById(R.id.listView);
+        ArrayAdapter<String> listViewAdapter = new ArrayAdapter<String>(
+                getActivity(),
+                android.R.layout.simple_list_item_1,
+                menuItems
+        );
+        listView.setAdapter(listViewAdapter);
+        return rootView;
     }
 }
