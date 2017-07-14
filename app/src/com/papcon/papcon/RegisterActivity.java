540f5e6b777abe319886ce1164760f4e54b9c6bb
kyj930419 <kyj930419@naver.com>
2017-07-14 22:35:33 +0900

    회원가입 구조 구성

--- app/src/com/papcon/papcon/RegisterActivity.java
+++ app/src/com/papcon/papcon/RegisterActivity.java
@@ -1,12 +1,19 @@
 package com.papcon.papcon;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -19,57 +26,92 @@ public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_register);
 
-        final EditText idText = (EditText) findViewById(R.id.idText);
-        final EditText passwordText = (EditText) findViewById(R.id.passwordText);
-        final EditText nameText = (EditText) findViewById(R.id.nameText);
-        final EditText ageText = (EditText) findViewById(R.id.ageText);
+            Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+            setSupportActionBar(toolbar);
 
-        Button registerButton = (Button) findViewById(R.id.registerButton);
+            final EditText idText = (EditText) findViewById(R.id.idText);
+            final EditText passwordText = (EditText) findViewById(R.id.passwordText);
+            final EditText nameText = (EditText) findViewById(R.id.nameText);
 
-        registerButton.setOnClickListener(new View.OnClickListener() {
+            Button registerButton = (Button) findViewById(R.id.registerButton);
+
+            final Spinner sexSpinner = (Spinner) findViewById(R.id.sexSpinner);
+            final ArrayAdapter sAdapter = ArrayAdapter.createFromResource(this,R.array.sex,R.layout.spinner_item);
+            sexSpinner.setAdapter(sAdapter);
+            sexSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                @Override
+                public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                    ((TextView) sexSpinner.getChildAt(0)).setTextColor(Color.WHITE);
+                }
+
+                @Override
+                public void onNothingSelected(AdapterView<?> parent) {
+
+                }
+            });
+
+
+        final Spinner ageSpinner = (Spinner) findViewById(R.id.ageSpinner);
+        final ArrayAdapter aAdapter = ArrayAdapter.createFromResource(this,R.array.age,R.layout.spinner_item);
+        ageSpinner.setAdapter(aAdapter);
+        ageSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
-            public void onClick(View v) {
-                String userID = idText.getText().toString();
-                String userPassword = passwordText.getText().toString();
-                String userName = nameText.getText().toString();
-                int userAge = Integer.parseInt(ageText.getText().toString()); //String을 integer형으로 형변환
-
-                Response.Listener<String> responseListner = new Response.Listener<String>(){
-                    @Override
-                    public void onResponse(String response){
-                        try{
-                            JSONObject jsonResponse = new JSONObject(response);
-                            boolean success = jsonResponse.getBoolean("success");
-                            if(success){
-                                AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
-                                builder.setMessage("회원 등록에 성공했습니다.")
-                                        .setPositiveButton("확인", null)
-                                        .create()
-                                        .show();
-                                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
-                                RegisterActivity.this.startActivity(intent);
-                            }
-                            else{
-                                AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
-                                builder.setMessage("회원 등록에 실패했습니다.")
-                                        .setNegativeButton("다시 시도", null)
-                                        .create()
-                                        .show();
-                            }
-
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                };
-                RegisterRequest registerRequest = new RegisterRequest(userID, userPassword, userName, userAge, responseListner);
-                RequestQueue queue = Volley.newRequestQueue(RegisterActivity.this);
-                queue.add(registerRequest);
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                ((TextView) ageSpinner.getChildAt(0)).setTextColor(Color.WHITE);
             }
-        });{
-        }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+
+
+            registerButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String userID = idText.getText().toString();
+                    String userPassword = passwordText.getText().toString();
+                    String userName = nameText.getText().toString();
+                    int userAge = Integer.parseInt(ageSpinner.getSelectedItem().toString()); //String을 integer형으로 형변환
+
+                    Response.Listener<String> responseListner = new Response.Listener<String>(){
+                        @Override
+                        public void onResponse(String response){
+                            try{
+                                JSONObject jsonResponse = new JSONObject(response);
+                                boolean success = jsonResponse.getBoolean("success");
+                                if(success){
+                                    AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
+                                    builder.setMessage("회원 등록에 성공했습니다.")
+                                            .setPositiveButton("확인", null)
+                                            .create()
+                                            .show();
+                                    Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                                    RegisterActivity.this.startActivity(intent);
+                                }
+                                else{
+                                    AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
+                                    builder.setMessage("회원 등록에 실패했습니다.")
+                                            .setNegativeButton("다시 시도", null)
+                                            .create()
+                                            .show();
+                                }
+
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    };
+                    RegisterRequest registerRequest = new RegisterRequest(userID, userPassword, userName, userAge, responseListner);
+                    RequestQueue queue = Volley.newRequestQueue(RegisterActivity.this);
+                    queue.add(registerRequest);
+                }
+            });{
+            }
     }
 }
