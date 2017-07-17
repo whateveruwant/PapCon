1f0e303bf57de4916abdb53dbd3a9532fe595985
kyj930419 <kyj930419@naver.com>
2017-07-18 00:15:38 +0900

    ㅂㄷㅂㄷ

--- app/src/com/papcon/papcon/RegisterActivity.java
+++ app/src/com/papcon/papcon/RegisterActivity.java
@@ -2,8 +2,8 @@ package com.papcon.papcon;
 
 import android.content.Intent;
 import android.graphics.Color;
+
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -18,6 +18,7 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.toolbox.Volley;
@@ -25,7 +26,11 @@ import com.android.volley.toolbox.Volley;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.regex.Pattern;
+
 public class RegisterActivity extends AppCompatActivity {
+
+    String checkedID="";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,6 +65,40 @@ public class RegisterActivity extends AppCompatActivity {
             }
         });
 
+        Button idcheckButton = (Button) findViewById(R.id.idcheckButton);
+
+        idcheckButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                final String userID = idText.getText().toString();
+
+                Response.Listener<String> reponseListener = new Response.Listener<String>() {
+
+                    @Override
+                    public void onResponse(String response) {
+                        try{
+                            JSONObject jsonObject = new JSONObject(response);
+                            boolean success = jsonObject.getBoolean("success");
+                            if(success){ // 중복
+                                Toast.makeText(RegisterActivity.this,"이미 사용중인 ID 입니다.", Toast.LENGTH_SHORT).show();
+
+                            }
+                            else{
+                                Toast.makeText(RegisterActivity.this,"사용 가능한 ID 입니다.",Toast.LENGTH_SHORT).show();
+                                checkedID = userID;
+                            }
+                        }
+                        catch (Exception e){
+                            e.printStackTrace();
+                        }
+                    }
+                };
+                IDcheckRequest idcheckRequest = new IDcheckRequest(userID, reponseListener);
+                RequestQueue queue = Volley.newRequestQueue(RegisterActivity.this);
+                queue.add(idcheckRequest);
+            }
+
+        });
 
         final Spinner ageSpinner = (Spinner) findViewById(R.id.ageSpinner);
         final ArrayAdapter aAdapter = ArrayAdapter.createFromResource(this,R.array.age,R.layout.spinner_item);
@@ -76,6 +115,7 @@ public class RegisterActivity extends AppCompatActivity {
             }
         });
 
+
         registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -88,6 +128,7 @@ public class RegisterActivity extends AppCompatActivity {
                 String userAddress = addressText.getText().toString();
                 String userInfo = introductionText.getText().toString();
 
+
                 Response.Listener<String> responseListner = new Response.Listener<String>(){
                     @Override
                     public void onResponse(String response){
@@ -95,16 +136,11 @@ public class RegisterActivity extends AppCompatActivity {
                             JSONObject jsonResponse = new JSONObject(response);
                             boolean success = jsonResponse.getBoolean("success");
                             if(success){
-                                AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
-                                builder.setMessage("회원 등록에 성공했습니다.")
-                                        .setPositiveButton("확인", null)
-                                        .create()
-                                        .show();
                                 Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
                                 RegisterActivity.this.startActivity(intent);
                             }
                             else{
-                                AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
+                                AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this,R.style.DialogStyle);
                                 builder.setMessage("회원 등록에 실패했습니다.")
                                         .setNegativeButton("다시 시도", null)
                                         .create()
@@ -117,9 +153,28 @@ public class RegisterActivity extends AppCompatActivity {
                     }
                 };
 
-                RegisterRequest registerRequest = new RegisterRequest(userType, userID, userPassword, userName, userSex, userAge, userAddress, userInfo, responseListner);
-                RequestQueue queue = Volley.newRequestQueue(RegisterActivity.this);
-                queue.add(registerRequest);
+
+                if(!checkedID.equals(userID)){
+                    Toast.makeText(RegisterActivity.this,"아이디 중복확인을 해주세요.",Toast.LENGTH_SHORT).show();
+
+                }
+                else if (!Pattern.matches("^[a-zA-Z0-9]*.{4,20}$",userID )){
+                    Toast.makeText(RegisterActivity.this,"아이디 형식이 아닙니다.",Toast.LENGTH_SHORT).show();
+
+                }
+                else if (!Pattern.matches("^[a-zA-Z0-9!@.#$%^&*?_~]{6,20}$", userPassword)){
+                    Toast.makeText(RegisterActivity.this,"비밀번호 형식이 아닙니다.",Toast.LENGTH_SHORT).show();
+
+                }
+
+                else {
+                    RegisterRequest registerRequest = new RegisterRequest(userType, userID, userPassword, userName, userSex, userAge, userAddress, userInfo, responseListner);
+                    RequestQueue queue = Volley.newRequestQueue(RegisterActivity.this);
+                    queue.add(registerRequest);
+
+                }
+
+
             }
         });{
         }
